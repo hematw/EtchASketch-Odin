5dad8453c77dde37c8b726541c809bed181f9686
Hemat Waziri <hematullahw@gmail.com>
2024-01-26 14:52:03 +0430

    Size input Add

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 const gridContainer = document.querySelector(".grid-container");
+const htmlRoot = document.querySelector(":root");
+const btn = document.querySelector("#btn");
+const sizeInput = document.querySelector("#size-input");
+const error = document.querySelector(".error")
+
+let size = getComputedStyle(htmlRoot).getPropertyValue("--size");
 
 function addDivs(num) {
-    num = num ** 2*4;
+    num = num ** 2;
     for (let i = 0; i < num; i++) {
         gridContainer.innerHTML += '<div class="grid-item"></div>'
     }
 }
-addDivs(20);
+addDivs(size);
 
 function randomColor() {
     let colorCode = '';
     for(let i = 0; i<6; i++) {
         colorCode += Math.floor(Math.random() * 16).toString(16);
     }
-
     return colorCode;
 }
 
+btn.addEventListener("click", ()=> {
+    if(!sizeInput.value || sizeInput.value>100) {
+        error.innerHTML = "Invalid input!"
+    } else {
+        gridContainer.innerHTML = "";
+        addDivs(sizeInput.value)
+    }
+})
 
 const gridItems = document.querySelectorAll(".grid-item");
 gridItems.forEach((item) => {
@@ -24,5 +37,3 @@ gridItems.forEach((item) => {
         item.setAttribute("style", `background-color:#${randomColor()}`)
     }));
 });
-
-
