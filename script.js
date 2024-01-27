48901ea4ec6093d2cb96d8771d13164246e30d9d
Hemat Waziri <hematullahw@gmail.com>
2024-01-27 21:34:05 +0430

    Performance bug fix

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
 const gridContainer = document.querySelector(".grid-container");
-const htmlRoot = document.querySelector(":root");
 const btn = document.querySelector("#btn");
 const sizeInput = document.querySelector("#size-input");
 const error = document.querySelector(".error")
+const htmlRoot = document.querySelector(":root");
 
-let size = getComputedStyle(htmlRoot).getPropertyValue("--size");
+let size = 40;
 
 function addDivs(num) {
-    num = num ** 2;
-    for (let i = 0; i < num; i++) {
-        gridContainer.innerHTML += '<div class="grid-item"></div>'
+    numSqr = num ** 2;
+    for (let i = 0; i < numSqr; i++) {
+        let div = document.createElement("div")
+        div.className = "grid-item";
+        div.style.width = `${560/num}px`
+        div.style.paddingBottom = `${560/num}px`
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor= `#${randomColor()}`;
+        });
+        gridContainer.appendChild(div);
     }
 }
 addDivs(size);
 
 function randomColor() {
     let colorCode = '';
-    for(let i = 0; i<6; i++) {
+    for (let i = 0; i < 6; i++) {
         colorCode += Math.floor(Math.random() * 16).toString(16);
     }
     return colorCode;
 }
 
-btn.addEventListener("click", ()=> {
-    if(!sizeInput.value || sizeInput.value>100) {
+btn.addEventListener("click", () => {
+    if (!sizeInput.value || sizeInput.value > 100) {
         error.innerHTML = "Invalid input!"
     } else {
+        htmlRoot.style.setProperty(--size, sizeInput.value);
         gridContainer.innerHTML = "";
         addDivs(sizeInput.value)
     }
 })
-
-const gridItems = document.querySelectorAll(".grid-item");
-gridItems.forEach((item) => {
-    item.addEventListener("mouseover", (() => {
-        item.setAttribute("style", `background-color:#${randomColor()}`)
-    }));
-});
